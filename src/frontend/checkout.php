3f3da5006eb32fa99da054b005adf42c6644d4a5
Marcello Stanisci <marcello.stanisci@inria.fr>
2015-10-22 18:42:16 +0200

    Adding specification of the custom "pay" URL inside the object sent along the 'contract' event.

--- src/frontend/checkout.php
+++ src/frontend/checkout.php
@@ -76,7 +76,10 @@
    pass it to the extension */
 function handle_contract(json_contract)
 {
-  var cEvent = new CustomEvent('taler-contract', { 'detail' : json_contract });
+  var cEvent = new CustomEvent('taler-contract',
+                             { detail: json_contract,
+			       target: "/taler/pay"});
+
   document.body.dispatchEvent(cEvent);
 };
 
