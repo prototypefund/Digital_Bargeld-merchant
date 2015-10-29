aafc4a5ab30da39132f0dda8862762bfa3e5e19d
Marcello Stanisci <marcello.stanisci@inria.fr>
2015-10-29 22:19:24 +0100

    adding deposit sample data for testing

---
+++ src/frontend/deposit.php
@@ -0,0 +1,26 @@
+<?php 
+/*
+  This file is part of GNU TALER.
+  Copyright (C) 2014, 2015 GNUnet e.V.
+
+  TALER is free software; you can redistribute it and/or modify it under the
+  terms of the GNU Lesser General Public License as published by the Free Software
+  Foundation; either version 2.1, or (at your option) any later version.
+
+  TALER is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+  A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License along with
+  TALER; see the file COPYING.  If not, If not, see <http://www.gnu.org/licenses/>
+
+*/
+
+$post_body = file_get_contents('php://input');
+
+$post_obj = json_decode($post_body);
+
+//debug and get test-ish data for the backend
+file_put_contents('/tmp/deposit_permission',
+                  json_encode($post_obj, JSON_PRETTY_PRINT));
+?>
