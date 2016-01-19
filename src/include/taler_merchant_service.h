09b3cfee78cebd84cfec64ce28cd5d39be0a12d2
Christian Grothoff <christian@grothoff.org>
2016-01-19 14:39:37 +0100

    -fix (C) notices

--- src/include/taler_merchant_service.h
+++ src/include/taler_merchant_service.h
@@ -1,6 +1,6 @@
 /*
   This file is part of TALER
-  Copyright (C) 2014, 2015 Christian Grothoff (and other contributing authors)
+  Copyright (C) 2014, 2015 GNUnet e.V.
 
   TALER is free software; you can redistribute it and/or modify it under the
   terms of the GNU Affero General Public License as published by the Free Software
