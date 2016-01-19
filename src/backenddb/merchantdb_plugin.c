5da68e54540be098f405ceef00fb648478bb73df
Christian Grothoff <christian@grothoff.org>
2016-01-19 14:39:19 +0100

    -fix (C) notices

--- src/backenddb/merchantdb_plugin.c
+++ src/backenddb/merchantdb_plugin.c
@@ -1,6 +1,6 @@
 /*
   This file is part of TALER
-  Copyright (C) 2015 Christian Grothoff (and other contributing authors)
+  Copyright (C) 2015 GNUnet e.V.
 
   TALER is free software; you can redistribute it and/or modify it under the
   terms of the GNU General Public License as published by the Free Software
