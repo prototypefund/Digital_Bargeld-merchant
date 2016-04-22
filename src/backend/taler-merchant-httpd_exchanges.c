2b54be2a33c7705197a44fb2cce8a4d81eff4e3e
Florian Dold <florian.dold@gmail.com>
2016-04-22 18:50:22 +0200

    excessive logging

--- src/backend/taler-merchant-httpd_exchanges.c
+++ src/backend/taler-merchant-httpd_exchanges.c
@@ -403,6 +403,9 @@ TMH_EXCHANGES_find_exchange (const char *chosen_exchange,
                   chosen_exchange);
       break;
     }
+    GNUNET_log (GNUNET_ERROR_TYPE_DEBUG,
+                "Comparing chosen exchange url '%s' with known url '%s'.",
+                chosen_exchange, exchange->uri);
   }
   if (NULL == exchange)
   {
