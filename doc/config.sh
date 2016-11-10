0ab4b9c2378f45e6a8a323e95173f83e04dff617
Marcello Stanisci <marcello.stanisci@inria.fr>
2016-11-10 18:14:07 +0100

    doc: copyright header

--- doc/config.sh
+++ doc/config.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
+# This file is part of TALER
+# (C) 2014, 2015, 2016 INRIA
+# TALER is free software; you can redistribute it and/or modify it under the
+# terms of the GNU Affero General Public License as published by the Free Software
+# Foundation; either version 3, or (at your option) any later version.
+# TALER is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License along with
+# TALER; see the file COPYING.  If not, see <http://www.gnu.org/licenses/>
+
+# @author Marcello Stanisci
+# @brief Walkthrough configuration steps
+
 echo -e "\nThis script will set and show all the configuration\n\
 values needed to run an example backend. The backend will listen on port 8888\n\
 and cooperate with exchange at https://exchange.demo.taler.net/.\n\
 Additionally, the script will also generate the backend's private\n\
 key and banking details' file.\n\n"
+
+# FIXME:
+#       1) banking details generator missing
 
 echo -n "Press ENTER to start > "
 read
@@ -80,7 +97,3 @@ echo -n "taler-config -s merchant-demoexchange -o master_key -V CQQZ9DY3MZ1ARMN5
 read
 echo 
 taler-config -s merchant-demoexchange -o master_key -V "CQQZ9DY3MZ1ARMN5K1VKDETS04Y2QCKMMCFHZSWJWWVN82BTTH00"
-
-# FIXME:
-#       1) put banking details generator
-#       2) test!
