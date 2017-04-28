aa18b6e28574c8e8bcd1628d871c8be5f7af060b
Marcello Stanisci <marcello.stanisci@inria.fr>
2017-04-28 11:19:59 +0200

    installing mitm via makefile

---
+++ src/mitm/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+setup(name='talermerchantmitm',
+      version='0.0',
+      description='Layer generating errors for testing',
+      url='git://taler.net/merchant',
+      author='Marcello Stanisci',
+      author_email='marcello.stanisci@inria.fr',
+      license='GPL',
+      packages=find_packages(),
+      install_requires=["Flask>=0.10"],
+      scripts=['taler-merchant-mitm'],
+      zip_safe=False)
