280aecd3d6e4a87373af2e36c86389799cffb5bf
Florian Dold <florian.dold@gmail.com>
2017-11-30 03:02:52 +0100

    add critical missing comma

--- src/backend/taler-merchant-httpd_tip-authorize.c
+++ src/backend/taler-merchant-httpd_tip-authorize.c
@@ -200,7 +200,7 @@ MH_handler_tip_authorize (struct TMH_RequestHandler *rh,
                                   TALER_EC_TIP_AUTHORIZE_INSUFFICIENT_FUNDS,
                                   "Insufficient funds for tip");
   }
-  json_t *tip_token = json_pack ("{s:o, s:o, s:s, s:s}"
+  json_t *tip_token = json_pack ("{s:o, s:o, s:s, s:s}",
                                  "tip_id", GNUNET_JSON_from_data_auto (&tip_id),
                                  "expiration", GNUNET_JSON_from_time_abs (expiration),
                                  "exchange_url", mi->tip_exchange,
