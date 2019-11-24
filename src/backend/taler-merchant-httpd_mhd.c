9c45d148ab574df055a5642dbd4c87deef0bef39
Christian Grothoff <christian@grothoff.org>
2019-11-24 16:58:44 +0100

    update header

--- src/backend/taler-merchant-httpd_mhd.c
+++ src/backend/taler-merchant-httpd_mhd.c
@@ -1,6 +1,6 @@
 /*
   This file is part of TALER
-  Copyright (C) 2014, 2015, 2016 GNUnet e.V. and INRIA
+  Copyright (C) 2014-2019 Taler Systems SA
 
   TALER is free software; you can redistribute it and/or modify it under the
   terms of the GNU Affero General Public License as published by the Free Software
